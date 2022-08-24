0787704f80f81713c3399713e7d173da22aeb9b6
Hxy <huxiaoyang1208@qq.com>
2022-08-24 10:52:38 +0800

    v0.0.2

--- main.cpp
+++ main.cpp
@@ -1,6 +1,23 @@
-#include <iostream>
-
+#include <bits/stdc++.h>
+#define ra register int
+using namespace std;
+int n,G[1005][4];
+vector<int> bian;
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    scanf("%d",&n);
+    for(ra i(1);i<=n;++i){
+        int x,y,z;
+        scanf("%d%d%d",&x,&y,&z);
+        G[i][1]=x,G[i][2]=y,G[i][3]=z;
+    }
+    for(ra i(1);i<=n;++i){
+        for(ra j(1);j<=n;++j) {
+            if(j==i){
+                continue;
+            }
+            int cost=min(abs(G[i][1]-G)
+            bian.push_back()
+        }
+    }
     return 0;
 }
