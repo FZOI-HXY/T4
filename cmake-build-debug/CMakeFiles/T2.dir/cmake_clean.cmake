5c6b0891a50b4e97afabfb846e6201cc8233c185
Hxy <huxiaoyang1208@qq.com>
2022-08-24 10:29:28 +0800

    v0.0.1

---
+++ cmake-build-debug/CMakeFiles/T2.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/T2.dir/main.cpp.obj"
+  "T2.exe"
+  "T2.exe.manifest"
+  "T2.pdb"
+  "libT2.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/T2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
