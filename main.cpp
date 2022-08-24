79be67866bd8c1632bc120e8af25232cd8e8ed8a
Hxy <huxiaoyang1208@qq.com>
2022-08-24 11:01:34 +0800

    v0.0.3

--- main.cpp
+++ main.cpp
@@ -1,10 +1,31 @@
 #include <bits/stdc++.h>
 #define ra register int
 using namespace std;
-int n,G[1005][4];
-vector<int> bian;
+int n,G[1005][4],fa[1005];
+long long ans=0;
+struct Node{
+    int x,y,cost;
+};
+vector<Node> bian;
+inline int find(int x){
+    while(x!=fa[x]){
+        x=fa[x]=fa[fa[x]];
+    }
+    return x;
+}
+inline void merge(int x,int y){
+    int rx=find(x),ry=find(y);
+    fa[rx]=ry;
+}
+inline void kruskal(){
+    sort(bian.begin(),bian.end());
+
+}
 int main() {
     scanf("%d",&n);
+    for(ra i(1);i<=n;++i){
+        fa[i]=i;
+    }
     for(ra i(1);i<=n;++i){
         int x,y,z;
         scanf("%d%d%d",&x,&y,&z);
@@ -15,9 +36,10 @@ int main() {
             if(j==i){
                 continue;
             }
-            int cost=min(abs(G[i][1]-G)
-            bian.push_back()
+            int cost=min(abs(G[i][1]-G[j][1]),min(abs(G[i][2]-G[j][2]),abs(G[i][3]-G[j][3])));
+            bian.push_back({i,j,cost});
         }
     }
+    kruskal();
     return 0;
 }
